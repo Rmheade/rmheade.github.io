a57561bc3a140871d044819b79697250ec0dca82
HimeSpider <105820973+HimeSpider@users.noreply.github.com>
2024-11-22 16:54:53 -0500

    Update index.html

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function beep(x) {
     var iframe = document.createElement("iframe")
     var html = "<body>Foo</body>"
+    document.getElementById('all').hidden = true
     iframe.src = x + encodeURI(html)
     document.body.appendChild(iframe)
-    console.log("iframe.contentWindow =", iframe.contentWindow)
   }  
